85aa0c4428dcac79b91b47e909a5d2566f671222
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2019-11-25 23:06:21 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,65 +3,34 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './styles/tailwind.css';
-import puerto from './img/puerto.jpg';
 
 ReactDOM.render(
-    <div class="flex flex-col items-center font-sans font-light">
-        <nav class="flex items-center justify-between rounded-lg h-16 bg-gray-600 mt-2 mx-auto w-4/5">
-            <h1 class="text-2xl font-semibold text-white mx-2">Monumentos de Oviedo</h1>
-            <button class="bg-midnightblue hover:bg-blue-700 text-white font-bold py-2 px-4 h-12 rounded-lg mx-2">
-                ↓ ≡
+    <div class="flex-col items-center">
+        <nav class="flex items-center justify-between rounded-full h-16 bg-gray-600 mt-2 mx-10 w-4/5">
+            <h1 class="text-3xl text-white mx-8">Hola</h1>
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-full mx-6">
+                ­­­­≡
             </button>
         </nav>
-            <div class="w-4/5 rounded-lg my-4 overflow-hidden shadow mx-auto hover:shadow-xl">
-                <img class="w-full" src={puerto} alt="Sunset in the mountains"></img>
+
+            <div class="w-4/5 rounded overflow-hidden shadow-lg mx-10 my.">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"></img>
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Mar</div>
+                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
                     <p class="text-gray-700 text-base">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                     </p>
                 </div>
                 <div class="px-6 py-4">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Ports Routes</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Sea</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 cursor-pointer my-1 mx-1">Another Example</span>
-
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
                 </div>
             </div>
-            <div class="w-4/5 rounded-lg my-4 overflow-hidden shadow mx-auto hover:shadow-xl">
-                <img class="w-full" src={puerto} alt="Sunset in the mountains"></img>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Puerto</div>
-                    <p class="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div class="px-6 py-4">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Ports Routes</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Sea</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 cursor-pointer my-1 mx-1">Another Example</span>
-
-                </div>
-            </div>
-            <div class="w-4/5 rounded-lg my-4 overflow-hidden shadow mx-auto hover:shadow-xl">
-                <img class="w-full" src={puerto} alt="Sunset in the mountains"></img>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Faro</div>
-                    <p class="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div class="px-6 py-4">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Ports Routes</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer my-1 mx-1">Sea</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 cursor-pointer my-1 mx-1">Another Example</span>
-
-                </div>
-            </div> 
     </div>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
