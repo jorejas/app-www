689148d7f7637250d335a33e394b89a7f2ae9356
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2019-11-26 22:21:16 +0100

    Change Menu

--- src/index.js
+++ src/index.js
@@ -7,11 +7,28 @@ import puerto from './img/puerto.jpg';
 
 ReactDOM.render(
     <div class="flex flex-col items-center font-sans font-light">
-        <nav class="flex items-center justify-between rounded-lg h-16 bg-gray-600 mt-2 mx-auto w-4/5">
-            <h1 class="text-2xl font-semibold text-white mx-2">Monumentos de Oviedo</h1>
-            <button class="bg-midnightblue hover:bg-blue-700 text-white font-bold py-2 px-4 h-12 rounded-lg mx-2">
-                ≡
-            </button>
+        <nav class="flex items-center w-4/5 my-4 justify-between rounded-lg flex-wrap bg-oviedoblue p-6">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+                <span class="font-semibold text-xl tracking-tight">Historia de Oviedo</span>
+            </div>
+            <div class="block lg:hidden">
+                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items- lg:w-auto">
+                <div class="text-sm lg:flex-grow">
+                <a href="#guerra-civil" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white mr-4">
+                    Guerra Civil
+                </a>
+                <a href="#Prerromanico" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white mr-4">
+                    Prerromanico
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white">
+                    Blog
+                </a>
+                </div>
+            </div>
         </nav>
             <div class="w-4/5 rounded-lg my-4 overflow-hidden shadow mx-auto hover:shadow-xl">
                 <img class="w-full" src={puerto} alt="Sunset in the mountains"></img>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        oviedoblue:'#004179',
+      },
+    }
+  },
+  variants: {},
+  plugins: []
+}
