bf88ba89c697b3b5de1c6097df050ca237b0cbfe
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2019-11-27 21:46:43 +0100

    Responsive

--- src/index.js
+++ src/index.js
@@ -5,31 +5,59 @@ import * as serviceWorker from './serviceWorker';
 import './styles/tailwind.css';
 import puerto from './img/puerto.jpg';
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+  
+          // Get the "main-nav" element
+          var $target = document.getElementById('main-nav');
+  
+          // Toggle the class on "main-nav"
+          $target.classList.toggle('hidden');
+  
+        });
+      });
+    }
+  
+  });
+
 ReactDOM.render(
     <div class="flex flex-col bg-gray-100 items-center font-sans font-light">
-        <nav class="flex items-center w-4/5 my-4 justify-between rounded-lg flex-wrap bg-oviedoblue p-6">
-            <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <span class="font-semibold text-xl tracking-tight">Historia de Oviedo</span>
-            </div>
-            <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
-            <div class="w-full block flex-grow lg:flex lg:items- lg:w-auto">
-                <div class="text-sm lg:flex-grow">
-                <a href="#guerra-civil" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white mr-4">
-                    Guerra Civil
-                </a>
-                <a href="#Prerromanico" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white mr-4">
-                    Prerrománico
-                </a>
-                <a href="#about" class="block mt-4 lg:inline-block lg:mt-0 text-white font-normal hover:text-white">
-                    About
-                </a>
+            <div class="border-b border-indigo-darkest bg-oviedoblue w-4/5 my-4 py-6 px-6 rounded-lg px-2">
+                <div class="container mx-auto">
+                    <nav class="flex items-center justify-between flex-wrap">
+                        <div class="flex items-center flex-no-shrink text-white mr-6">
+                            <span class="font-semibold text-white text-xl tracking-tight">Historia de Oviedo</span>
+                        </div>
+                        <div class="block sm:hidden">
+                            <button class="navbar-burger flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+                                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                            </button>
+                        </div>
+                        <div id="main-nav" class="w-full leading-none flex-grow sm:flex items-center sm:w-auto hidden">
+                            <div class="text-sm sm:flex-grow">
+                                <a href="#" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-white hover:text-grey-300 mr-4">
+                                   Guerra Civil
+                                </a>
+                                <a href="#" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-white hover:text-grey-300 mr-4">
+                                   Prerrománico
+                                </a>
+                                <a href="#" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-white hover:text-grey-300 mr-4">
+                                   About
+                                </a>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
-        </nav>
             <div class="w-4/5 rounded-lg bg-white my-4 overflow-hidden shadow mx-auto hover:shadow-xl">
                 <img class="w-full" src={puerto} alt="Sunset in the mountains"></img>
                 <div class="px-6 py-4">
@@ -77,7 +105,7 @@ ReactDOM.render(
             </div>
 
 
-
+            <footer></footer>
             <footer class="flex-center mb-4 mx-auto align-center"><p>Creado por: <a class="m-0 underline" target="_blank" href="https://jorgeorejas.com">Jorge Orejas</a></p></footer>
     </div>
 , document.getElementById('root'));
