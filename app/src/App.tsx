bcdaba5ad9a2632dcd14cf0df0e7a10f26683599
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2020-02-24 19:17:40 +0100

    .

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-} from '@ionic/react';
+import Menu from './components/Menu';
+import Page from './pages/Index';
+import Tours from './pages/Tours'
+import React, { useState } from 'react';
+import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import {homeOutline, starOutline, informationCircleOutline} from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { Redirect, Route } from 'react-router-dom';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,33 +25,26 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={homeOutline} />
-            <IonLabel>Main</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={starOutline} />
-            <IonLabel>Favourites</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={informationCircleOutline} />
-            <IonLabel>Infos</IonLabel>
-        </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+
+  const [selectedPage, setSelectedPage] = useState('');
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonSplitPane contentId="main">
+          <Menu selectedPage={selectedPage} />
+          <IonRouterOutlet id="main">
+            <Route path="/page/:name" render={(props) => {
+              setSelectedPage(props.match.params.name);
+              return <Page {...props} />;
+            }} exact={true} />
+            <Route path="/" render={() => <Redirect to="/page/Index" />} exact={true} />
+          </IonRouterOutlet>
+        </IonSplitPane>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
