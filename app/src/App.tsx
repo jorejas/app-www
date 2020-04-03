eacb1d0466cefc4f4d864c1bb3ac66738fe8f025
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2020-04-03 17:39:33 +0200

    _redirects

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, IonButton } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonButton, IonPage } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
 /* Paginas */
@@ -26,36 +26,17 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-/* Slides */
-
-import { IonSlides, IonSlide, IonContent } from '@ionic/react';
-
-// Parameters
-
-const slideOpts = {
-  effect: "fade",
-  autoHeight: true,
-  initialSlide: 0,
-  speed: 400,
-  autoplay: {
-    delay: 2500,
-    stopOnLastSlide: false,
-  },
-  scrollbar:{
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-};
-
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true}/>
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Route path="/hola" component={Hola} exact={true}/>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonPage>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/home" component={Home} exact={true}/>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/hola" component={Hola} exact={true}/>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonPage>
   </IonApp>
 );
 
