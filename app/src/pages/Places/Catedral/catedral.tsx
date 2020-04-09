a5de03838398aa9a8325de5953de08f4e5f4d7f7
Jorge Orejas <40031461+jorejas@users.noreply.github.com>
2020-04-09 11:49:41 +0200

    Fixed images

--- app/src/pages/Places/Catedral/catedral.tsx
+++ app/src/pages/Places/Catedral/catedral.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { IonCard , IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg, IonSlides, IonSlide, useIonViewDidEnter } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonCard , IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg, IonSlides, IonSlide, IonButton, IonPopover, IonText } from '@ionic/react';
 import mandala from './img/mandala.jpg';
 import nave from './img/nave.jpg';
 import torre from './img/torre.jpg';
@@ -13,23 +12,34 @@ const slidesOpts = {
 };
 
 const Catedral: React.FC = () => {
+    const [showPopover, setShowPopover] = useState(false);
     return(
-        <IonCard color="light">
-            <IonSlides pager={true} options={slidesOpts}>
-                <IonSlide><IonImg src={mandala}/></IonSlide>
-                <IonSlide><IonImg src={nave}/></IonSlide>
-                <IonSlide><IonImg src={torre}/></IonSlide>
-                <IonSlide><IonImg src={mosaico}/></IonSlide>
-                <IonSlide><IonImg src={vistas}/></IonSlide>
-            </IonSlides>
-            <IonCardHeader>
-              <IonCardTitle>Catedral</IonCardTitle>
-              <IonCardSubtitle>Esto es una prueba</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam quod modi laudantium quibusdam autem nesciunt ipsa repudiandae reiciendis voluptas hic quo, perferendis eligendi fuga et nostrum officiis sit. Aut.
-            </IonCardContent>
-        </IonCard>
+        <div>
+          <IonCard color="light">
+              <IonSlides pager={true} options={slidesOpts}>
+                  <IonSlide><IonImg src={mandala}/></IonSlide>
+                  <IonSlide><IonImg src={nave}/></IonSlide>
+                  <IonSlide><IonImg src={torre}/></IonSlide>
+                  <IonSlide><IonImg src={mosaico}/></IonSlide>
+                  <IonSlide><IonImg src={vistas}/></IonSlide>
+              </IonSlides>
+              <IonCardHeader>
+                <IonCardTitle>Catedral</IonCardTitle>
+                <IonCardSubtitle>Esto es una prueba</IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam quod modi laudantium quibusdam autem nesciunt ipsa repudiandae reiciendis voluptas hic quo, perferendis eligendi fuga et nostrum officiis sit. Aut.
+              </IonCardContent>
+                <IonButton expand="block" onClick={() => setShowPopover(true)}>→</IonButton>
+          </IonCard>
+          <IonPopover
+            isOpen={showPopover}
+            animated= {true}
+            show-backdrop={true}
+            onDidDismiss={e => setShowPopover(false)}>
+              <p>Esto es una prueba</p>
+            </IonPopover>
+        </div>
     )
 }
 
